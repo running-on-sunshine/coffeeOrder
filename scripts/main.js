757eb8baacf4990b47c6e82267143a492d704888
Liz Khuu <khuu.liz@gmail.com>
2018-07-31 13:29:33 -0400

    Fixed remove function to update order array in Local Storage

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,27 @@
-var coffee = document.querySelector('[name="coffee"]');
-var email = document.querySelector('[name="emailAddress"]');
-var flavorShot = document.querySelector('[name="flavor"]');
-var strengthLevel = document.querySelector('[name="strength"]');
-var incomingOrders = document.querySelector('.incomingOrders');
-
 var orders = [];
 
-var createOrder = function() {
-    var row = document.createElement('div');
-    row.classList.add('coffeeOrder');
-    var size = document.querySelector('[name="size"]:checked');
-    var coffeeOrder = `${coffee.value}, email: ${email.value}, ${size.value}, ${flavorShot.value}, ${strengthLevel.value}`;
-    row.textContent = coffeeOrder;
-    orders.push(coffeeOrder);
+var saveOrder = function () {
+    localStorage.setItem('coffee-orders', JSON.stringify(orders));
+};
 
+var createOrder = function() {
     var orderCompletedSection = document.createElement('div');
     orderCompletedSection.classList.add('orderCompletedSection');
+
+    var checkOrders = [];
+
+    var coffee = document.querySelector('[name="coffee"]');
+    var email = document.querySelector('[name="emailAddress"]');
+    var flavorShot = document.querySelector('[name="flavor"]');
+    var strengthLevel = document.querySelector('[name="strength"]');
+    var size = document.querySelector('[name="size"]:checked');
+    var incomingOrders = document.querySelector('.incomingOrders');
+    var coffeeOrder = `${coffee.value}, email: ${email.value}, ${size.value}, ${flavorShot.value}, ${strengthLevel.value}`;
+
+    var row = document.createElement('div');
+    row.classList.add('coffeeOrder');
+    row.textContent = coffeeOrder;
+    orders.push(coffeeOrder);
 
     var createButton = function () {
         var completeButton = document.createElement('button');
@@ -26,19 +32,22 @@ var createOrder = function() {
         completeButton.addEventListener('click', removeOrder);
     };
 
-    var saveOrder = function () {
-        localStorage.setItem('coffee-orders', JSON.stringify(orders));
-    };
-
     var removeOrder = function () {
         incomingOrders.removeChild(row);
-    }
+        orders.forEach(function (order) {
+            if (order !== coffeeOrder) {
+                checkOrders.push(order);
+            }
+        })
+        orders = checkOrders;
+        saveOrder(orders);
+    };
 
     row.appendChild(orderCompletedSection);
     incomingOrders.appendChild(row);
 
     createButton();
-    saveOrder();
+
 };
 
 var coffeeOrderForm = document.querySelector('.coffeeOrderForm');
@@ -46,12 +55,23 @@ var coffeeOrderForm = document.querySelector('.coffeeOrderForm');
 coffeeOrderForm.addEventListener('submit', function(event) {
     event.preventDefault();
     createOrder();
+    saveOrder();
 });
+
+// var displayOrder = function () {
+//     for (var i = 0; i < orders.length; i++) {
+//         console.log(orders[i]);
+//         coffeeOrderString = orders[i];
+//         row.appendChild(coffeeOrderString);
+//         incomingOrders.appendChild(row);
+// };
 
 // var retrieveOrder = function () {
 //     var orderString = localStorage.getItem('coffee-orders');
-//     orders = JSON.parse(orderString);
-//     for (var i = 0; i < orders.length; i++) {
-//         createOrder(orders[i]);
+//     if (orderString !== null) {
+//         orders = JSON.parse(orderString);
+//         console.log(orders);
+//         displayOrder();
+//         }
 //     }
 // };
